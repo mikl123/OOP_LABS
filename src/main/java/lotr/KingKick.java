7a29428f1157c8dccff5acfa5a3fc5082a40b0a5
mikl123 <bulesnmisha3@gmil.com>
2023-10-07 12:22:44 +0300

    Test

--- src/main/java/lotr/KingKick.java
+++ src/main/java/lotr/KingKick.java
@@ -6,7 +6,7 @@ public class KingKick implements KickStr {
  private static Random rand = new Random();
     @Override
     public void kick(Character a, Character b) {
-        int hp_dec = b.getHp() - rand.nextInt(0, a.getPower());
+        int hp_dec = b.getHp() - rand.nextInt( a.getPower());
         b.setHp(hp_dec);
     }
     
