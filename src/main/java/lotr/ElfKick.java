10cb67566f846dc4ff33a36ae55a7327cda60f54
mikl123 <bulesnmisha3@gmil.com>
2023-10-07 12:40:01 +0300

    Test

--- src/main/java/lotr/ElfKick.java
+++ src/main/java/lotr/ElfKick.java
@@ -4,7 +4,7 @@ public class ElfKick implements KickStr{
 
     @Override
     public void kick(Character a, Character b) {
-        if (b.getPower() < a.getPower()){
+        if (b.getPower() <= a.getPower()){
             b.setHp(0);
            }else{
             a.power-=1;
